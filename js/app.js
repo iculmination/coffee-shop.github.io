5b5ca86652d8a5cd03456ad9e78becaab15bf173
iculmination <97855851+iculmination@users.noreply.github.com>
2023-12-28 20:05:55 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 console.log("app js");
+var myForm = document.querySelector(".email-form");
+
+myForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // обробка форми
+});
+
